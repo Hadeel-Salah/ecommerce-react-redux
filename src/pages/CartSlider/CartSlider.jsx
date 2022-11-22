2d36f5d8e28e09e448c211e1a5796d6ee3cef3ab
hadeelsala7 <h.salah.technical@gmail.com>
2022-11-22 23:19:11 +0200

    Create CartSlider.jsx

---
+++ src/pages/CartSlider/CartSlider.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { ListGroup } from 'reactstrap';
+import { cartUiActions } from '../../store/shopping-cart/cartUiSlice'
+import { CartItem } from './CartItem';
+import './CartSlider.css';
+
+
+export const CartSlider = () => {
+    const dispatch = useDispatch();
+    const cartProducts = useSelector((state) => state.cart.cartItems);
+    const totalAmount = useSelector((state) => state.cart.totalAmount);
+
+    const toggleCart = () => {
+        dispatch(cartUiActions.toggle());
+    };
+    return (
+        <div className="cart__container">
+            <ListGroup className="cart">
+                <div className="cart__close">
+                    <span onClick={toggleCart}>
+                        <i class="ri-close-fill"></i>
+                    </span>
+                </div>
+
+                <div className="cart__item-list">
+                    {cartProducts.length === 0 ? (
+                        <h6 className="text-center mt-5">No item added to the cart</h6>
+                    ) : (
+                        cartProducts.map((item, index) => (
+                            <CartItem item={item} key={index} />
+                        ))
+                    )}
+                </div>
+
+                <div className="cart__bottom d-flex align-items-center justify-content-between">
+                    <h6>
+                        Subtotal : <span>${totalAmount}</span>
+                    </h6>
+                    <button>
+                        <Link to="/checkout" onClick={toggleCart}>
+                            Checkout
+                        </Link>
+                    </button>
+                </div>
+            </ListGroup>
+        </div>
+    );
+};
