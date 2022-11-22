94fe5ff0997e32fc593e0a2ff5dcae37b28c77ee
hadeelsala7 <h.salah.technical@gmail.com>
2022-11-22 23:19:31 +0200

    Create featuresData.jsx

---
+++ src/mockdata/featuresData.jsx
@@ -0,0 +1,22 @@
+
+import featureImg01 from "../assets/images/service-01.png";
+import featureImg02 from "../assets/images/service-02.png";
+import featureImg03 from "../assets/images/service-03.png";
+export const featuresData = [
+    {
+      title: "Quick Delivery",
+      imgUrl: featureImg01,
+      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus, doloremque.",
+    },
+  
+    {
+      title: "Super Dine In",
+      imgUrl: featureImg02,
+      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus, doloremque.",
+    },
+    {
+      title: "Easy Pick Up",
+      imgUrl: featureImg03,
+      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus, doloremque.",
+    },
+  ];
