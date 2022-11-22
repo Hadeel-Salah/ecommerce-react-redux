d24f306b5a0d3aa0d92a7c29148ea895b1aab2bb
hadeelsala7 <h.salah.technical@gmail.com>
2022-11-22 23:18:48 +0200

    Create Food.jsx

---
+++ src/pages/Foods/Food.jsx
@@ -0,0 +1,93 @@
+import React, { useState } from 'react'
+import { Container, Row, Col } from 'reactstrap'
+import CommonSection from '../../components/CommonSection/CommonSection'
+import Helemet from '../../components/Helemet/Helemet'
+import { ProductCard } from '../../components/ProductCard/ProductCard'
+import products from '../../mockdata/products'
+import './food.css'
+import ReactPaginate from "react-paginate";
+
+
+export const Food = () => {
+    const [searchTerm, setSearchTerm] = useState("");
+    const [pageNumber, setPageNumber] = useState(0);
+    const searchedProduct = products.filter((item) => {
+        if (searchTerm.value === "") {
+            return item;
+        }
+        if (item.title.toLowerCase().includes(searchTerm.toLowerCase())) {
+            return item;
+        } else {
+            return console.log("not found");
+        }
+    });
+
+    const productPerPage = 12;
+    const visitedPage = pageNumber * productPerPage;
+
+    const displayPage = searchedProduct.slice(
+        visitedPage,
+        visitedPage + productPerPage
+    );
+
+    const pageCount = Math.ceil(searchedProduct.length / productPerPage);
+
+    const changePage = ({ selected }) => {
+        setPageNumber(selected);
+    };
+    return (
+        <Helemet title="All Foods">
+            <CommonSection title="All Foods" />
+            <section>
+                <Container>
+                    <Row>
+                        <Col lg="6" md="6" sm="6" xs="12">
+                            <div className="search__widget d-flex align-items-center justify-content-between ">
+                                <input
+                                    type="text"
+                                    placeholder="I'm looking for...."
+                                    value={searchTerm}
+                                    onChange={(e) => setSearchTerm(e.target.value)}
+
+                                />
+                                <span>
+                                    <i class="ri-search-line"></i>
+                                </span>
+                            </div>
+                        </Col>
+                        <Col lg="6" md="6" sm="6" xs="12" className="mb-5">
+                            <div className="sorting__widget text-end">
+                                <select className="w-50">
+                                    <option>Default</option>
+                                    <option value="ascending">Alphabetically, A-Z</option>
+                                    <option value="descending">Alphabetically, Z-A</option>
+                                    <option value="high-price">High Price</option>
+                                    <option value="low-price">Low Price</option>
+                                </select>
+                            </div>
+                        </Col>
+                        {displayPage.map((item) => (
+                            <Col lg="3" md="4" sm="6" xs="6" key={item.id} className="mb-4">
+                                <ProductCard item={item} />
+                            </Col>
+                        ))}
+
+                        <div className="d-flex justify-content-center mt-5">
+                            <ReactPaginate
+                                itemClass="page-item"
+                                linkClass="page-link"
+                                pageCount={pageCount}
+                                onPageChange={changePage}
+                                previousLabel={"Prev"}
+                                nextLabel={"Next"}
+                                containerClassName=" paginationBttns "
+                            />
+                        </div>
+                    </Row>
+                </Container>
+
+            </section>
+
+        </Helemet>
+    )
+}
