03e990f908abe63a29858c54548f2dcd97b92c29
hadeelsala7 <h.salah.technical@gmail.com>
2022-11-22 23:16:53 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useSelector } from "react-redux";
+import { Footer } from "./components/Footer/Footer";
+import { Header } from "./components/Header/Header";
+import { CartSlider } from "./pages/CartSlider/CartSlider";
+import { PagesRouters } from './routers/PagesRouters';
 
 function App() {
+  const showCart = useSelector((state) => state.cartUi.cartIsVisible)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <Header />
+        {showCart && <CartSlider />}
+        <PagesRouters />
+        <Footer />
+    </>
+
   );
 }
 
