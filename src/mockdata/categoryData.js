a7aca8d28c69e2fb20ad50793ead40f25283b795
hadeelsala7 <h.salah.technical@gmail.com>
2022-11-22 23:19:29 +0200

    Create categoryData.js

---
+++ src/mockdata/categoryData.js
@@ -0,0 +1,26 @@
+
+import categoryImg01 from "../assets/images/category-01.png";
+import categoryImg02 from "../assets/images/category-02.png";
+import categoryImg03 from "../assets/images/category-03.png";
+import categoryImg04 from "../assets/images/category-04.png";
+
+export const categoryData = [
+    {
+        display: "Fastfood",
+        imgUrl: categoryImg01,
+    },
+    {
+        display: "Pizza",
+        imgUrl: categoryImg02,
+    },
+
+    {
+        display: "Asian Food",
+        imgUrl: categoryImg03,
+    },
+
+    {
+        display: "Row Meat",
+        imgUrl: categoryImg04,
+    },
+];
