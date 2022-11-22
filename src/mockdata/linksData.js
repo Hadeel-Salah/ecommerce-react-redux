cf230de1e4c3c8c19721fdb883c9fead73d5cb6d
hadeelsala7 <h.salah.technical@gmail.com>
2022-11-22 23:19:27 +0200

    Create linksData.js

---
+++ src/mockdata/linksData.js
@@ -0,0 +1,14 @@
+export const linksData = [
+    {
+        title: "Home",
+        path: "/home",
+    },
+    {
+        title: "Foods",
+        path: "/foods",
+    },
+    {
+        title: "Cart",
+        path: "/cart",
+    },
+];
